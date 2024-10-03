8c211dc5f929c93d29f8622dc72913999d416234
Jino <lacsajino14@gmail.com>
2024-10-03 19:44:18 +0800

    Created attractions and functionality for adding new attractions

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
-test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+test_traveler = ['Erin Wilkes', 'Cairo, Egypt', ['historical site', 'art']]
 
 def get_destination_index(destination):
 	destination_index = destinations.index(destination)
@@ -14,3 +14,15 @@ def get_traveler_location(traveler):
 	return traveler_destination_index
 test_destination_index = get_traveler_location(test_traveler)
 print(test_destination_index)
+
+attractions = [[] for destination in destinations]
+print(attractions)
+
+def add_attraction(destination, attraction):
+	destination_index = get_destination_index(destination)
+	if destination in destinations:
+		attractions_for_destination = attractions[destination_index]
+		attractions_for_destination.append(attraction)
+	return attractions_for_destination
+add_attraction("Los Angeles, USA", ['Venice Beach', ['beach']])
+print(attractions)
